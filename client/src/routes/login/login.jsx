d3c4ba55ba87e14c9b17c0381c8809aaa4e288f1
DanishAliUmar <danishaliumar4@gmail.com>
2024-05-27 13:03:40 +0500

    complete Login/Logout/Register

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import "./login.scss";
-import { Link } from "react-router-dom";
-
+import { Link, json, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
+import axios from 'axios'
 function Login() {
+  const [error, setError] = useState("")
+  const [isloading, setIsloading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    setIsloading(true)
+    setError("")
+    const formData = new FormData(e.target);
+
+    const username = formData.get('username')
+    const password = formData.get('password')
+
+    try {
+      const res = await apiRequest.post("/auth/login",{
+        username,password
+      })
+
+      localStorage.setItem("user", JSON.stringify(res.data))
+
+      navigate("/")
+    } catch (error) {
+      setError(error.response.data.message)
+    }finally{
+      setIsloading(false)
+    }
+  }
+
   return (
     <div className="login">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
+          <input name="username" required minLength={3} maxLength={20} type="text" placeholder="Username" />
+          <input name="password" type="password" required placeholder="Password" />
+          <button disabled={isloading}>Login</button>
+          {error && <span>{error}</span>}
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,16 +1,48 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import axios from 'axios'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
+  const [error, setError] = useState("")
+  const [isloading, setIsloading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    setIsloading(true)
+    setError("")
+    const formData = new FormData(e.target);
+
+    const username = formData.get('username')
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    try {
+      const res = await apiRequest.post("/auth/register",{
+        username,email,password
+      })
+
+      navigate("/login")
+    } catch (error) {
+      setError(error.response.data.message)
+    }finally{
+      setIsloading(false)
+    }
+  }
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isloading}>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
